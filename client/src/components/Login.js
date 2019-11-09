2c6e1fb3c7f2b4c250ea0b7ae095424528864dc9
Carlos <miklo.concepcion@gmail.com>
2019-11-08 18:52:35 -0600

    client and server side are rendering after npm install. establishing login page as basically the home/landing page for the project. answering questions in readme. 3 more to go.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,11 +2,20 @@ import React from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
+  // const login = () => {
+  //   axios.post('endpoint/here', userCredentials)
+  //     .then(res => {
+  //       localStorage.setItem('token', res.data.token);
+  //       props.history.push('/dashboard');
+  //     }
+  // }
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      {/* <p>Build a login page here</p> */}
+      {/* Building login page form. */}
+      <button>Login</button>
     </>
   );
 };
