f7d62401ca0fd9232cbe25f13fae79380a904e3b
Carlos <miklo.concepcion@gmail.com>
2019-11-08 19:49:10 -0600

    questions answered. finishing login. testing api endpoints. Continuing on authentication. Steps - - [ ] Construct an AXIOS request to retrieve a token from the server. You'll use this token to interact with the API - [ ] Save the token to localStorage - [ ] Build a  module to create an instance of axios with the authentication header - [ ] Build a  component and use it to protect a route that renders the  component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { axiosWithAuth } from "../../../server";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
-  // const login = () => {
-  //   axios.post('endpoint/here', userCredentials)
-  //     .then(res => {
-  //       localStorage.setItem('token', res.data.token);
-  //       props.history.push('/dashboard');
-  //     }
-  // }
+
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       {/* <p>Build a login page here</p> */}
-      {/* Building login page form. */}
+      
+      {/*  login page form. */}
+      <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      
       <button>Login</button>
     </>
   );
